27f4df5a2217ce96edf983747b45510c65b92577
rmitkov <rmitkov@ozon.ru>
2022-07-16 16:37:18 +0700

    4

--- task03-map.go
+++ task03-map.go
@@ -9,7 +9,6 @@ func sortMapValues(input map[int]string) (result []string) {
 		ids = append(ids, id)
 	}
 	sort.Ints(ids)
-	result := make([]string, len(ids)-1)
 	for _, value := range ids {
 		result = append(result, value)
 	}
